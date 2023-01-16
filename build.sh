d885ba045df84730c298bd7f6c664ea3507e5d59
tyrionchen <tyrionchen@tencent.com>
2023-01-16 16:01:28 +0800

    增加构建脚本

---
+++ build.sh
@@ -0,0 +1,18 @@
+# 运行这个脚本的前提:
+# 1.需要先确保能正常构建ALXR客户端(参考官网或:https://iwiki.woa.com/pages/viewpage.action?pageId=4007072608)
+# 2.需要手动安装cargo-ndk: cargo install cargo-ndk
+
+set -e
+
+rm -rf alxr_lib
+mkdir alxr_lib
+
+# 编译原始alxr客户端
+mkdir -p alxr_lib/official/arm64-v8a
+cargo xtask build-alxr-android --oculus-quest
+cp ./target/quest/debug/apk/lib/arm64-v8a/*.so alxr_lib/official/arm64-v8a/
+
+# 编译定制版alxr客户端
+mkdir -p alxr_lib/tcr/arm64-v8a
+cargo xtask build-tcr-version
+cp target/quest/arm64-v8a/*.so alxr_lib/tcr/arm64-v8a/
